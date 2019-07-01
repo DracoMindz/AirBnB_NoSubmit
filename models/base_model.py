830372e891e7bb6abf05b912a560c0800ff5e702
Jonathan Vasquez <727@holberton.com>
2019-07-01 18:46:56 +0000

    added documentation (needs work)

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,15 @@ from datetime import datetime
 
 class BaseModel:
     """
-    defines all common attributes/methods for BaseModel classes
+    public instance attrs for BaseModel go here
     """
     def __init__(self, *args, **kwargs):
         """
-        public instance attributes go here
+        Initializes a new instance of BaseModel
         """
 
         timeFormat = "%Y-%m-%dT%H:%M:%S.%f"
         if len(kwargs) == 0:
-            """
-            Initializes a new instance of BaseModel
-            """
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
